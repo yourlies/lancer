0e7e9b4dcb29cdd04d1f5a647c89a034490ecf6c
lies <958142428@qq.com>
2021-01-15 18:04:59 +0800

    save

---
+++ typings/item.shim.d.ts
@@ -0,0 +1,14 @@
+interface InsetItem {
+  text: string;
+  type: string;
+  month: string;
+  day: string;
+  time: string;
+  complete: boolean;
+}
+interface InputItem {
+  text: string;
+  type: string;
+  time: string;
+  date: string;
+}
